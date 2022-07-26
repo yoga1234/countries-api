f700ba7d469c33999fbbc2bb3f16fcfdcd25a999
Septiana Yoga <septianayoga30@gmail.com>
2022-07-26 15:55:54 +0700

    navbar styling

--- src/assets/index.js
+++ src/assets/index.js
@@ -1,3 +1,4 @@
 import moonOutline from "./moon-outline.svg";
+import moonOutlineWhite from "./moon-outline-white.svg";
 
-export { moonOutline };
+export { moonOutline, moonOutlineWhite };
--- src/components/navbar/Navbar.js
+++ src/components/navbar/Navbar.js
@@ -1,14 +1,14 @@
 import React from "react";
 
 import "./Navbar.css";
-import { moonOutline } from "../../assets";
+import { moonOutline, moonOutlineWhite } from "../../assets";
 
 const Navbar = () => {
   return (
     <div className="navbar-container">
       <h1>Where in the world?</h1>
       <div className="dark-toggle">
-        <img src={moonOutline} alt="" />
+        <img src={moonOutlineWhite} alt="" />
         <p>Dark Mode</p>
       </div>
     </div>
