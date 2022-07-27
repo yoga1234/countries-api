62c242a758a053a8fe9905765e7b34ecb12f9f13
Septiana Yoga <septianayoga30@gmail.com>
2022-07-27 15:17:16 +0700

    styling for search and filter bar

--- src/components/searchAndFilter/SearchAndFilter.js
+++ src/components/searchAndFilter/SearchAndFilter.js
@@ -1,28 +1,30 @@
 import React from "react";
 
 import "./SearchAndFilter.css";
-import { searchOutline } from "../../assets";
+import { searchOutline, searchOutlineWhite } from "../../assets";
 
 const SearchAndFilter = () => {
   return (
     <div className="search-container">
       <div className="search-bar">
         <form>
-          <img src={searchOutline} alt="" />
+          <img src={searchOutlineWhite} alt="" />
           <input type="text" placeholder="Search for a country..." />
         </form>
       </div>
       <div className="filter-bar">
-        <select name="regionfilter">
-          <option value="" disabled selected hidden>
-            Filter by Region
-          </option>
-          <option value="Africa">Africa</option>
-          <option value="Africa">America</option>
-          <option value="Africa">Asia</option>
-          <option value="Africa">Europe</option>
-          <option value="Africa">Oceania</option>
-        </select>
+        <div className="select-wrapper">
+          <select name="regionfilter">
+            <option value="" disabled selected hidden>
+              Filter by Region
+            </option>
+            <option value="Africa">Africa</option>
+            <option value="Africa">America</option>
+            <option value="Africa">Asia</option>
+            <option value="Africa">Europe</option>
+            <option value="Africa">Oceania</option>
+          </select>
+        </div>
       </div>
     </div>
   );
