50f38ab5040825292fe0a49af33fc60c76a049c7
Septiana Yoga <septianayoga30@gmail.com>
2022-07-29 17:20:37 +0700

    fix minor

--- src/components/countries/country/Country.js
+++ src/components/countries/country/Country.js
@@ -3,7 +3,23 @@ import React from "react";
 import "./Country.css";
 
 const Country = () => {
-  return <div>Country</div>;
+  return (
+    <div className="country-container">
+      <div className="top-side">{/* <img src="" alt="" /> */}</div>
+      <div className="bottom-side">
+        <h2>Country</h2>
+        <p>
+          Population: <span>1.000.000</span>
+        </p>
+        <p>
+          Region: <span>Europe</span>
+        </p>
+        <p>
+          Capital: <span>Berlin</span>
+        </p>
+      </div>
+    </div>
+  );
 };
 
 export default Country;
