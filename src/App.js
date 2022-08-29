43decb4e50cef7ae1ef86bd89ead4b23db6eeebe
Septiana Yoga <septianayoga30@gmail.com>
2022-08-29 16:09:45 +0700

    setting up API call for the data

--- src/App.js
+++ src/App.js
@@ -1,14 +1,15 @@
-import { useState } from "react";
+import { useState, useEffect } from "react";
 import "./App.css";
 import {
   Navbar,
-  // SearchAndFilter,
-  // CountryContainer,
-  SingleCountry,
+  SearchAndFilter,
+  CountryContainer,
+  // SingleCountry,
 } from "./components/index";
 
 const App = () => {
   const [darkMode, setDarkMode] = useState(true);
+  const [countryData, setCountryData] = useState();
   const themeToggle = () => {
     if (darkMode === true) {
       document.body.classList.toggle("light-mode");
@@ -20,12 +21,23 @@ const App = () => {
     }
   };
 
+  const getData = () => {
+    fetch("https://restcountries.com/v3.1/all")
+      .then((res) => res.json())
+      .then((data) => {
+        setCountryData(data);
+      });
+  };
+  useEffect(() => {
+    getData();
+  }, []);
+
   return (
     <div className="App">
       <Navbar toggle={themeToggle} darkMode={darkMode} />
-      {/* <SearchAndFilter darkMode={darkMode} /> */}
-      {/* <CountryContainer /> */}
-      <SingleCountry darkMode={darkMode} />
+      <SearchAndFilter darkMode={darkMode} />
+      <CountryContainer />
+      {/* <SingleCountry darkMode={darkMode} /> */}
     </div>
   );
 };
