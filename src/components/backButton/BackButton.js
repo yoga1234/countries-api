a1e9dedcb6ca7d84a2024b6b2d91c6a009f65261
Septiana Yoga <septianayoga30@gmail.com>
2022-08-31 15:55:31 +0700

    fix back button styling issues

--- src/components/backButton/BackButton.js
+++ src/components/backButton/BackButton.js
@@ -6,17 +6,16 @@ import "./BackButton.css";
 const BackButton = ({ darkMode, toggleContent }) => {
   return (
     <div className="single-back-btn">
-      <img
-        className="backward-icon"
-        src={darkMode === true ? backwardWhite : backwardDark}
-        alt=""
-      />
-      {/* <img className="backward-icon" src={backwardWhite} alt="" /> */}
       <button
         onClick={() => {
           toggleContent("single");
         }}
       >
+        <img
+          className="backward-icon"
+          src={darkMode === true ? backwardWhite : backwardDark}
+          alt=""
+        />
         Back
       </button>
     </div>
